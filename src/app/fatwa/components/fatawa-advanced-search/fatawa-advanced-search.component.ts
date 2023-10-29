ae61ebd68e8ead5c0ab98fa6e5394d85c74125e2
Mahmoud-Abdelmoughiss <abdelmoughiss@gmail.com>
2023-10-29 21:43:43 +0200

    add fatwa advanced search before department section

--- src/app/fatwa/components/fatawa-advanced-search/fatawa-advanced-search.component.ts
+++ src/app/fatwa/components/fatawa-advanced-search/fatawa-advanced-search.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fatawa-advanced-search',
   templateUrl: './fatawa-advanced-search.component.html',
   styleUrls: ['./fatawa-advanced-search.component.css']
 })
-export class FatawaAdvancedSearchComponent {
+export class FatawaAdvancedSearchComponent implements AfterViewInit, OnInit {
+
+  windowWidth:number = 0;
+  pageHeaderObj:any = {
+    title:'البحث المتقدم',
+    hasSubTitle : false,
+    subtitle:'',
+    total:0
+  };
+  sectionHeaderObj:any = {
+    title:'الأقسام',
+    hasSubTitle : false,
+    subtitle:'',
+    total:0
+  };
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event) {
+    this.windowWidth = window.innerWidth;
+  }
+  ngAfterViewInit(): void {
+    this.windowWidth = window.innerWidth;
+  }
+  ngOnInit(): void {
+    this.windowWidth = window.innerWidth;
+  }
 
 }
