ade2ad55afccbc8655165dc2e44c36426c5f1be2
Mahmoud Hassan <abosamour990@gmail.com>
2023-11-12 00:57:37 +0200

    merge

--- src/app/video-and-audio/components/video-audio/video-audio.component.ts
+++ src/app/video-and-audio/components/video-audio/video-audio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { HomeService } from 'src/app/home/core/services/home.service';
 
 @Component({
@@ -7,22 +8,31 @@ import { HomeService } from 'src/app/home/core/services/home.service';
   styleUrls: ['./video-audio.component.css']
 })
 export class VideoAudioComponent implements OnInit{
-
-  constructor(private service:HomeService) {
+  myForm: any;
+  constructor(private service:HomeService,private fb: FormBuilder) {
   }
-  items:any[]=[]
+  items:any[]=[]  
+  itemAll:any[]=[]
   ngOnInit(): void {
     this.getData()
+    this.myForm = this.fb.group({
+      search: [''],
+      type:[''],
+      type2:[''],
+      special:['']
+    })
+   
   }
-
   getData() {
     this.service.getCourses(1, 1000).subscribe(res => {
       const items1 = res.data.items;
       this.service.getLessons(1, 1000).subscribe(res2 => {
         const items2 = res2.data.items;
-        console.log(res2.data.items);
+        // console.log(res2.data.items);
+        
         
         this.items = this.interleaveArrays(items1, items2);
+        this.itemAll=this.items
       });
     });
   }
@@ -52,5 +62,50 @@ export class VideoAudioComponent implements OnInit{
   w3_close() {
     this.sidebarDisplay = 'none';
     this.overlayDisplay = 'none';
+  }
+  submit(){
+    var search=this.itemAll
+   if(this.myForm.get('search').value !=""){
+    this.items=[]
+    this.itemAll.forEach((e:any)=>{
+      if(e.title.includes(this.myForm.get('search').value)){
+        this.items.push(e)
+      }
+    })
+   }
+   else{
+    this.items=this.itemAll
+   }
+
+   
+   if(this.myForm.get('type').value && this.myForm.get('type2').value){
+    
+   }else{
+   if(this.myForm.get('type').value){
+    if(this.myForm.get('search').value!=""){
+      search=this.items
+      this.items=[]
+     }
+    search.forEach((e:any)=>{
+      if(e.type='lession'){
+        this.items.push(e)
+      }
+    })
+   }
+   if(this.myForm.get('type2').value){
+    if(this.myForm.get('search').value!=""){
+      search=this.items
+      this.items=[]
+     }
+    search.forEach((e:any)=>{
+      if(e.type='course'){
+        this.items.push(e)
+      }
+    })
+   }
+  }
+
+  
+   
   }
 }
