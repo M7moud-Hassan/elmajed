417f65a56f9d573b86948e765b703bc6975adc87
Mahmoud-Abdelmoughiss <abdelmoughiss@gmail.com>
2023-11-12 23:19:33 +0200

    add some changes

--- src/app/shared/components/app-loader/app-loader.component.ts
+++ src/app/shared/components/app-loader/app-loader.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, HostListener, OnChanges, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, HostListener, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { AppLoaderService } from '../../core/services/app-loader.service';
 
 @Component({
@@ -8,27 +8,14 @@ import { AppLoaderService } from '../../core/services/app-loader.service';
 })
 export class AppLoaderComponent implements OnInit {
   showSpinner = false;
-  windowHeight: number;
-  windowWidth: number;
+  constructor(private spinnerService: AppLoaderService, private cdRef: ChangeDetectorRef) {}
 
-  constructor(private spinnerService: AppLoaderService, private cdRef: ChangeDetectorRef) {
-    this.windowHeight = window.innerHeight;
-    this.windowWidth = window.innerWidth;
-  }
-
-  @HostListener('window:resize', ['$event'])
-onResize(event:any) {
-  this.windowHeight = window.innerHeight;
-  this.windowWidth = window.innerWidth;
-}
   ngOnInit() {
     this.init();
   }
-  
   init() {
     this.spinnerService.getSpinnerObserver().subscribe((status) => {
       this.showSpinner = (status === 'start');
-      // alert(status);
       this.cdRef.detectChanges();
     });
   }
