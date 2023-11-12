03ad7bc8f74c948a7e12dc27a77a56fc4343e2a3
Mahmoud-Abdelmoughiss <abdelmoughiss@gmail.com>
2023-11-12 23:52:31 +0200

    add some fixes

--- src/app/video-and-audio/components/courses-content/courses-content.component.ts
+++ src/app/video-and-audio/components/courses-content/courses-content.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HomeService } from 'src/app/home/core/services/home.service';
 import { DialogVideoImageComponent } from 'src/app/shared/components/dialog-video-image/dialog-video-image.component';
+import { PaginationVM } from 'src/app/shared/core/models/pagination-vm';
 
 @Component({
   selector: 'app-courses-content',
@@ -9,7 +10,19 @@ import { DialogVideoImageComponent } from 'src/app/shared/components/dialog-vide
   styleUrls: ['./courses-content.component.css']
 })
 export class CoursesContentComponent implements OnInit {
-  id:any
+  id:any;
+  archives:any[] = [];
+  pageNumber:number = 1;
+  PageSize:number = 1000;
+  total:number = 0;
+  selectedItems:any[]=[];
+  paginationObj: PaginationVM = {
+    count: 0,
+    total: 0,
+    current_page: 0,
+    per_page: 0,
+    total_pages: 0,
+  };
   constructor(private route:ActivatedRoute,private service:HomeService,
     ) {
    route.params.subscribe(res=>{
@@ -28,14 +41,32 @@ export class CoursesContentComponent implements OnInit {
 
   getData(id:any){
     this.service.getDetails(id).subscribe(res=>{
-      this.item=res.data.item
-      console.log("asdddddddd");
-      
-      console.log(this.item);
-      
+      this.item=res.data.item;
+      this.archives = this.item.archives;
+      this.onPageNumberClicked(1); 
     })
   }
 
+  onPageNumberClicked(pageNumber: number) {
+    this.pageNumber = pageNumber;
+    this.getElementsByPageSize(pageNumber,10);
+  }
+
+  getElementsByPageSize(currentPage:number,pageSize: number ){
+    let length = this.archives.length;
+    const pageCount = Math.ceil(length / pageSize);
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = Math.min(startIndex + pageSize, length);
+    const elements = this.archives.slice(startIndex, endIndex);
+    this.selectedItems = elements;
+    this.paginationObj = {
+      total: length ,
+      count: elements.length,
+      per_page: pageSize,
+      current_page: currentPage,
+      total_pages: pageCount
+    };
+  }
   
   
 }
